d1234bd65465fe18012944f6df6c25d8251b42e4
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-18 01:30:01 -0300

    Update keygen.py

--- keygenme228/keygen.py
+++ keygenme228/keygen.py
@@ -1,5 +1,13 @@
+#Crackme Info
+#Name: OldSoft's KeyGenMe #2 -- Upgraded from DOS
+#URL: https://crackmes.one/crackme/5c9e187b33c5d4419da55648
+#Author: wolverine2k
+#Language: C/C++
+#Platform: Windows
+#Level: 1
+#------------------
 #Min serial length: 3
-#The next character is greater than the current character
+#The next character is greater or equal than the current character
 import random
 import string
 
